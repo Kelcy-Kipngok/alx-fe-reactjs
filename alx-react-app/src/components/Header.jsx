cd96a1526f7b488cf8c0195726c6e39e4ab1394b
Kelcy <kelcykipngok@gmail.com>
2025-08-30 08:34:18 +0300

    Update Header.jsx

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -1,4 +1,4 @@
-function header() {
+function Header() {
     return (
         <div><header>
   <h1>My Favorite Cities</h1>
@@ -7,5 +7,5 @@ function header() {
     );
 }
 
-export default header
+export default Header
  
