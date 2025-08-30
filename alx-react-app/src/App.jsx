eaa2ec4a29cc58b9e3b9df3bb780fe46d9c11e22
Kelcy <kelcykipngok@gmail.com>
2025-08-30 08:43:56 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './Header'
 import MainContent from './MainContent'
 import Footer from './Footer'
+import UserProfile from "./components/UserProfile";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -9,7 +10,6 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <WelcomeMessage 
@@ -17,6 +17,12 @@ function App() {
       <MainContent />
       <Footer />
       <div>
+        <h1>Welcome to the User Profile Page</h1>
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
