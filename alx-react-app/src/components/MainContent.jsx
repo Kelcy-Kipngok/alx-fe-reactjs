4aa22fbab3fefa61590ce30462673a6942db725c
Kelcy <kelcykipngok@gmail.com>
2025-08-30 08:32:11 +0300

    Update MainContent.jsx

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,3 +1,12 @@
-<main>
+
+function MainComponent() {
+    return (
+        <div>
+         <main>
   <p>I love to visit New York, Paris, and Tokyo.</p>
 </main>
+        </div>
+    );
+}
+
+export default MainComponent;
