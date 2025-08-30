b9714d8077e79b3319c1675825490ea01f76d73f
Kelcy <kelcykipngok@gmail.com>
2025-08-30 08:47:05 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [count, setCount] = useState(0)
   return (
     <>
-      <WelcomeMessage 
+      <WelcomeMessage></WelcomeMessage>
         <Header />
       <MainContent />
       <Footer />
