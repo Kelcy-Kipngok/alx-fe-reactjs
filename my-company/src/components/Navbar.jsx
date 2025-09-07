f1726b646b7a8d6832ffbc37ffc1fbc58bd3b3b0
Kelcy <kelcykipngok@gmail.com>
2025-09-07 10:18:20 +0300

    Update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
+    "display", "justifyContent"
     <nav style={{ backgroundColor: '#222', padding: '10px' }}>
       <Link to="/" style={{ margin: '0 10px', color: 'white' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 10px', color: 'white' }}>About</Link>
