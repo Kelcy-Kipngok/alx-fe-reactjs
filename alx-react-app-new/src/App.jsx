809228bc309882ac987aea2b24516bfca4862fc3
Kelcy <kelcykipngok@gmail.com>
2025-09-07 09:14:12 +0300

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,9 +7,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Counter from './components/Counter';
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
   return (
     <>
       <WelcomeMessage> <WelcomeMessage />
@@ -29,6 +29,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+         <Counter />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
