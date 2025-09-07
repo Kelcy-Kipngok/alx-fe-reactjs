a0d61afc4bc937ee58b9ca69e10fbe1fbd2566ad
Kelcy <kelcykipngok@gmail.com>
2025-09-07 09:05:13 +0300

    Update UserProfile.jsx

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,10 +1,12 @@
+export default UserProfile;
+function UserProfile({ name, age, bio }) {
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '8px' }}>
+      <h2 style={{ color: 'blue' }}>{name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold' }}>{age}</span></p>
+      <p>Bio: {bio}</p>
     </div>
   );
-};
+}
 
 export default UserProfile;
