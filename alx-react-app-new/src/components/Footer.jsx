9bcd655e8ca71a102d6edd9acb615953f7797c2e
Kelcy <kelcykipngok@gmail.com>
2025-09-07 09:07:17 +0300

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,12 +1,9 @@
-
- function Footer () {
-    return (
-        <div>
-       <footer>
-  <p>© 2023 City Lovers</p>
-</footer>
-        </div>
-    );
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px' }}>
+      <p>© 2025 My React App</p>
+    </footer>
+  );
 }
 
 export default Footer;
