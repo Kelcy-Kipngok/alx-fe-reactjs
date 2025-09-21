88f911330ec85c482e6e617cf121a2da312370d8
Kelcy <kelcykipngok@gmail.com>
2025-09-21 15:33:38 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,5 +1,15 @@
 import axios from "axios";
 
+export const searchUsers = async (username, location, minRepos) => {
+  let query = username ? `${username}` : "";
+  if (location) query += `+location:${location}`;
+  if (minRepos) query += `+repos:>=${minRepos}`;
+
+  const res = await axios.get(`https://api.github.com/search/users?q=${query}`);
+  return res.data;
+};
+import axios from "axios";
+
 export const fetchUserData = async (username) => {
   const res = await axios.get(`https://api.github.com/users/${username}`);
   return res.data;
