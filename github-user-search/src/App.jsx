13823c66fc203edf45d2c696a8dce80398887ea6
Kelcy <kelcykipngok@gmail.com>
2025-09-21 15:26:54 +0300

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,6 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
 
 function App() {
   const [count, setCount] = useState(0)
