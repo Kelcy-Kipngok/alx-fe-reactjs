5c1656dd045d3f91e0aee121607ce8c5937a8168
Kelcy <kelcykipngok@gmail.com>
2025-09-28 10:29:54 +0300

    Update App.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,8 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
+
   const [count, setCount] = useState(0)
 
   return (
